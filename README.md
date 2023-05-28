fb3702ee561b575194e3f4a75fd47de3c7609166
Lucas <lucascomaru20@gmail.com>
2023-05-28 20:03:36 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # pytools2
 Refazendo pytools
+
+Módulo para exemplificar construção de projetos Python no curso Pytools
+
+Nesse curso é ensinado como contribuir com projetos de código aberto
+
+Suportada versão 3 de python
+
+Para instalar:
+
+'''console
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements-dev.txt
+'''
+
+Para conferir qualidade do código:
+
+'''console
+flake8
+'''
+
+
---
+++ libpythonpro/github_api.py
@@ -0,0 +1,22 @@
+import requests
+
+def buscar_avatar(usuario):
+    '''
+    Busca o avatar de um usuário no Github
+    
+    :para usuario: str com o nome de usuário no github
+    return: str com link do avatar
+    '''
+
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    data = resp.json()
+    
+    if 'avatar_url' in data:
+        return data['avatar_url']
+    else:
+        return 'Avatar não encontrado'
+
+if __name__ == '__main__':
+    print(buscar_avatar('lucascomaru'))
+
